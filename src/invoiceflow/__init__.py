c10f9736dc1e1d7ada20daac32b7229c0633e171
Garima Gupta <ggupta2@uncc.edu>
2026-09-22 18:55:48 -0400

    Export reminder planning APIs and sync version

--- src/invoiceflow/__init__.py
+++ src/invoiceflow/__init__.py
@@ -15,6 +15,22 @@ from .models import (
     money,
 )
 from .output import write_output
+from .reminder_policy import (
+    MAX_POLICY_BYTES,
+    MAX_UPCOMING_DAYS,
+    ReminderPolicy,
+    format_policy_json,
+    load_reminder_policy,
+    policy_from_dict,
+    policy_to_dict,
+)
+from .reminder_report import format_reminder_plan, reminder_plan_to_dict
+from .reminders import (
+    ReminderAction,
+    ReminderPlan,
+    ReminderState,
+    plan_reminders,
+)
 from .report import (
     due_review_to_dict,
     format_due_review,
@@ -25,7 +41,7 @@ from .report import (
 from .service import InvoiceService
 from .storage import MAX_LEDGER_INVOICES, InvoiceLedger
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 __all__ = [
     "DueInvoice",
@@ -37,17 +53,30 @@ __all__ = [
     "InvoiceLine",
     "InvoiceService",
     "InvoiceStatus",
+    "MAX_POLICY_BYTES",
+    "MAX_UPCOMING_DAYS",
+    "ReminderAction",
+    "ReminderPlan",
+    "ReminderPolicy",
+    "ReminderState",
     "MAX_LEDGER_INVOICES",
     "due_review_to_dict",
     "format_due_review",
     "format_invoice",
     "format_invoice_json",
     "format_invoice_list",
+    "format_policy_json",
+    "format_reminder_plan",
     "invoice_from_dict",
     "invoice_summary_to_dict",
     "invoice_to_dict",
     "load_invoice",
+    "load_reminder_policy",
     "money",
+    "plan_reminders",
+    "policy_from_dict",
+    "policy_to_dict",
+    "reminder_plan_to_dict",
     "review_due",
     "write_output",
     "__version__",
