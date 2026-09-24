cba42f36b4ec9dd7074499f3fac599bed0db88eb
Garima Gupta <ggupta2@uncc.edu>
2026-09-24 19:00:20 -0400

    Export backup APIs and sync runtime version

--- src/invoiceflow/__init__.py
+++ src/invoiceflow/__init__.py
@@ -9,6 +9,17 @@ from .aging import (
     classify_aging,
 )
 from .aging_report import aging_to_dict, format_aging
+from .backup import (
+    MAX_BACKUP_BYTES,
+    BackupSummary,
+    VerifiedBackup,
+    backup_from_dict,
+    build_backup,
+    create_backup,
+    load_backup,
+    restore_backup,
+)
+from .backup_report import backup_summary_to_dict, format_backup_summary
 from .due import DueInvoice, DueReview, DueState, review_due
 from .invoice_io import (
     format_invoice_json,
@@ -48,13 +59,20 @@ from .report import (
     invoice_summary_to_dict,
 )
 from .service import InvoiceService
-from .storage import MAX_LEDGER_INVOICES, InvoiceLedger
+from .storage import (
+    MAX_LEDGER_INVOICES,
+    InvoiceLedger,
+    format_ledger_json,
+    ledger_from_dict,
+    ledger_to_dict,
+)
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 
 __all__ = [
     "AgingAmount",
     "AgingBucket",
+    "BackupSummary",
     "CurrencyAging",
     "ReceivablesAging",
     "DueInvoice",
@@ -66,27 +84,38 @@ __all__ = [
     "InvoiceLine",
     "InvoiceService",
     "InvoiceStatus",
+    "MAX_BACKUP_BYTES",
     "MAX_POLICY_BYTES",
     "MAX_UPCOMING_DAYS",
     "ReminderAction",
     "ReminderPlan",
     "ReminderPolicy",
     "ReminderState",
+    "VerifiedBackup",
     "MAX_LEDGER_INVOICES",
     "age_receivables",
+    "backup_from_dict",
+    "backup_summary_to_dict",
+    "build_backup",
     "aging_to_dict",
     "classify_aging",
+    "create_backup",
     "due_review_to_dict",
     "format_aging",
+    "format_backup_summary",
     "format_due_review",
     "format_invoice",
     "format_invoice_json",
     "format_invoice_list",
+    "format_ledger_json",
     "format_policy_json",
     "format_reminder_plan",
     "invoice_from_dict",
     "invoice_summary_to_dict",
     "invoice_to_dict",
+    "ledger_from_dict",
+    "ledger_to_dict",
+    "load_backup",
     "load_invoice",
     "load_reminder_policy",
     "money",
@@ -94,6 +123,7 @@ __all__ = [
     "policy_from_dict",
     "policy_to_dict",
     "reminder_plan_to_dict",
+    "restore_backup",
     "review_due",
     "write_output",
     "__version__",
